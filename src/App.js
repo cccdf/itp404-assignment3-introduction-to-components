338010ab118c25824f3197adee7f8b95a2628d03
Dingfang <568157677@qq.com>
2019-09-12 00:28:58 -0700

    need subcribers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import getSubReddit from'./RedditApi';
+import  SubReddit from './SubReddit';
+import Loading from './Loading.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      results: [],
+      loading: true
+    };
+  };
+
+async componentDidMount(){
+  let response = await getSubReddit();
+  let results = response.data.children;
+  this.setState({results: results, loading: false});
 }
+
+render(){
+    return(
+      <div>
+        <p>{this.state.results.length} Results of NBA subreddit</p>
+        <p>subreddit subcribers: {this.state.results.subreddit_subscribers}</p>
+        <div>
+          {this.state.loading? <Loading></Loading>: this.state.results.map((result) => {
+            return <SubReddit result={result} key={result.id}></SubReddit>
+          })}
+        </div>
+      </div>
+    );
+  }
+
+
+
+
+}
+
+  
+
 
 export default App;
---
+++ src/SubReddit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function SubReddit(props) {
+  let result = props.result;
+  let num_comments = result.data.num_comments? result.data.num_comments.toLocaleString() : 'No comments!';
+  return <div className='result'>
+      <a href={result.data.url} target='_blank'>{result.data.title}</a>
+      <p>Score: {result.data.score}</p>
+      <p>Ups: {result.data.ups.toLocaleString()}</p>
+      <p>Comments: {num_comments}</p>
+      <p>Subreddit_Subcribers: {result.data.subreddit_subscribers}</p>
+  </div>;
+}
